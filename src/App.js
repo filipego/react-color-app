a9e4918b1c3b6399733c034d9966628f00870584
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2019-09-19 23:35:20 -0400

    generated shades of colors function

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
+}
+
+export default App;
